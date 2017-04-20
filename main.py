317a18b63495f3d4c65940b495b814b0cb328491
Simon R Klaver <simonrklaver@gmail.com>
2017-04-20 17:36:02 +0200

    I broke it

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from stemming.porter2 import stem
 import numpy as np
+import _thread as thr
 import csv
 from nltk.corpus import stopwords
 
-def stemquestion(question):
-  stemmedquestion = []
-  for word in question.split():
-    if word not in stopwords.words('english'):
-      stemmedquestion.append(stem(word.lower()))
-    
-  return stemmedquestion  
+class Hoi:
 
-def main():
-  with open("data/train.csv", "r") as f:
-    truesims = []
-    falsesims = []
-    
-    reader = csv.reader(f)
-    i = 0
-    for row in reader:
-      q1 = stemquestion(row[3])
-      q2 = stemquestion(row[4])
-      if row[5] == "1":
-        print(q1, q2, "\n\n")
+   def __init__(self, datafile):
+      self.truesims = []
+      self.falsesims = []
+      self.datafile = datafile
+
+   def stemquestion(self, question):
+     stemmedquestion = []
+     for word in question.split():
+       if word not in stopwords.words('english'):
+         stemmedquestion.append(stem(word.lower()))
+       
+     return stemmedquestion
+     
+   def similarityQuestions(self, row):
+      q1 = self.stemquestion(row[3])
+      q2 = self.stemquestion(row[4])
+      print("row: {0}, q1: {1}, q2: {2}".format(row, q1, q2))
+      #if row[5] == "1":
+      #  print(q1, q2, "\n\n")
       
       # Bekijk de similarity tussen de twee questions, zie wat een goede cut-off zou zijn #
       sim = 0
@@ -30,10 +32,25 @@ def main():
         if word in q2:
           sim += 1
       if row[5] == 1:
-        truesims.append(sim/len(q1))
+        self.truesims.append(sim/len(q1))
       else:
-        falsesims.append(sim/len(q1))
-                   
+        self.falsesims.append(sim/len(q1))
+
+
+   def run(self):
+     with open(self.datafile, "r") as f:
+       
+       reader = csv.reader(f)
+       i = 0
+       for row in reader:
+         try:
+            thr.start_new_thread(self.similarityQuestions, (row,))                  
+            #self.similarityQuestions(row)
+         except:
+            print("Error: THE END IS COMING")
       
 if __name__ == "__main__":
-  main()
+   datafile = "data/train.csv"
+   hoi = Hoi(datafile)
+   print(hoi.truesims)
+   print(hoi.falsesims)
